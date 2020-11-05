978200f81888a3b71b4cf455fcfec6673b8a1c42
DanielRNichols <danielrnichols63@gmail.com>
2020-11-05 07:22:13 -0600

    Added Validation to BudgetItemType

--- BudgetPlannerUI/App/Models/BudgetItemType.cs
+++ BudgetPlannerUI/App/Models/BudgetItemType.cs
@@ -1,6 +1,7 @@
 ﻿using BudgetPlannerUI.Interfaces;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
@@ -10,7 +11,11 @@ namespace BudgetPlannerUI.Models
     public class BudgetItemType : IDbResource
     {
         public int Id { get; set; }
+
+        [Required]
+        [StringLength(20, ErrorMessage = "Name is too long.")]
         public string Name { get; set; }
+
         public bool IsExpense { get; set; }
 
         //public virtual IList<BudgetItemGroup> BudgetItemGroups { get; set; }
